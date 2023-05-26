04958a1c68b95b3ea29bbf870526f3e0e1050616
vaishnavisawant57 <80047521+vaishnavisawant57@users.noreply.github.com>
2023-05-26 17:04:48 +0530

    Revert "added repoImpl and serviceImpl"
    This reverts commit 6c6258d8a7662b8e5830b38e603c2c6340c9a97b.

--- backend/clearingfeed/src/main/java/com/demo/clearingfeed/repo/TransactionRepo.java
+++ backend/clearingfeed/src/main/java/com/demo/clearingfeed/repo/TransactionRepo.java
@@ -1,12 +1,10 @@
 package com.demo.clearingfeed.repo;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.demo.clearingfeed.entity.Transaction;
+
 public interface TransactionRepo extends JpaRepository<Transaction,String> {
-	public void save(MultipartFile file);
-	public List<Transaction> getAllTransactions();
+
+	
 }
