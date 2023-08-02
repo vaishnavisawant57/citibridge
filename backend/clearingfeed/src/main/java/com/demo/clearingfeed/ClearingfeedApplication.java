b2efb9c74a4400e352daeeb7e6105e7b211e59c7
anushkaerande <99139073+anushkaerande@users.noreply.github.com>
2023-08-02 23:40:36 +0530

    edited clearing feed generation

--- backend/clearingfeed/src/main/java/com/demo/clearingfeed/ClearingfeedApplication.java
+++ backend/clearingfeed/src/main/java/com/demo/clearingfeed/ClearingfeedApplication.java
@@ -2,8 +2,10 @@ package com.demo.clearingfeed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.security.servlet.*;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class ClearingfeedApplication {
 
 	public static void main(String[] args) {
