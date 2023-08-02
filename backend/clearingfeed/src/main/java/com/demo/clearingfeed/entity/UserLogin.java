be2e9f2d9f09110a1e49e9efc5f749751cde7dd4
anushkaerande <99139073+anushkaerande@users.noreply.github.com>
2023-08-02 23:44:33 +0530

    added userlogin table

---
+++ backend/clearingfeed/src/main/java/com/demo/clearingfeed/entity/UserLogin.java
@@ -0,0 +1,78 @@
+package com.demo.clearingfeed.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="User")
+public class UserLogin {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String name;
+	private String email;
+	private String password;
+	
+	
+	
+	 public UserLogin(Long id, String name, String email, String password) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		
+	}
+	 
+	 
+
+	  public UserLogin() {
+		
+	}
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "UserLogin [id=" + id + ", name=" + name + ", email=" + email + ", password=" 
+	    + password + "]";
+	}
+	
+	
+	
+	}
